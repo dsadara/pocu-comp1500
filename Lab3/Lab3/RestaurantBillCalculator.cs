3b15e72073182f55f831140ec2cbbe5bb0ba73c4
lim971230 <lim971230@gmail.com>
2020-05-18 10:58:16 +0000

    Upload Lab3 files

---
+++ Lab3/Lab3/RestaurantBillCalculator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+
+namespace Lab3
+{
+    public static class RestaurantBillCalculator
+    {
+        public static double CalculateTotalCost(StreamReader input)
+        {
+            double tax, tip, foodTotalCost = 0, tempTotal;
+            double foodCost1 = double.Parse(input.ReadLine());
+            double foodCost2 = double.Parse(input.ReadLine());
+            double foodCost3 = double.Parse(input.ReadLine());
+            double foodCost4 = double.Parse(input.ReadLine());
+            double foodCost5 = double.Parse(input.ReadLine());
+            double tipPercent = double.Parse(input.ReadLine());
+
+            tax = foodCost1 * 0.05;
+            tip = (foodCost1 + tax) * tipPercent / 100;
+            tempTotal = foodCost1 + tax + tip;
+            foodTotalCost += tempTotal;
+
+            tax = foodCost2 * 0.05;
+            tip = (foodCost2 + tax) * tipPercent / 100;
+            tempTotal = foodCost2 + tax + tip;
+            foodTotalCost += tempTotal;
+
+            tax = foodCost3 * 0.05;
+            tip = (foodCost3 + tax) * tipPercent / 100;
+            tempTotal = foodCost3 + tax + tip;
+            foodTotalCost += tempTotal;
+
+            tax = foodCost4 * 0.05;
+            tip = (foodCost4 + tax) * tipPercent / 100;
+            tempTotal = foodCost4 + tax + tip;
+            foodTotalCost += tempTotal;
+
+            tax = foodCost5 * 0.05;
+            tip = (foodCost5 + tax) * tipPercent / 100;
+            tempTotal = foodCost5 + tax + tip;
+            foodTotalCost += tempTotal;
+
+            return foodTotalCost;
+        }
+
+        public static double CalculateIndividualCost(StreamReader input, double totalCost)
+        {
+            double costPerPeople;
+            double People = double.Parse(input.ReadLine());
+
+            costPerPeople = totalCost / People;
+            costPerPeople = Math.Round(costPerPeople, 2);
+
+            return costPerPeople;
+        }
+
+        public static uint CalculatePayerCount(StreamReader input, double totalCost)
+        {
+            uint PayerCount;
+            double costPerPayer = double.Parse(input.ReadLine());
+
+            PayerCount = (uint)Math.Ceiling(totalCost / costPerPayer);
+            return PayerCount;
+        }
+    }
+}
