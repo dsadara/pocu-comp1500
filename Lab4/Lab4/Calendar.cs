2b075a6cd8a30e70eabd77d3500f573e46bac321
lim971230 <lim971230@gmail.com>
2020-05-25 11:23:21 +0000

    Change year 0 to leap year

--- Lab4/Lab4/Calendar.cs
+++ Lab4/Lab4/Calendar.cs
@@ -4,10 +4,6 @@
     {
         public static bool IsLeapYear(uint year)
         {
-            if (year == 0)
-            {
-                return false;
-            }
             if (year % 4 == 0)
             {
                 if (year % 100 == 0)
