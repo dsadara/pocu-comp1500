e0fb358114d340062269c7d57cc3f0efbba057b6
lim971230 <lim971230@gmail.com>
2020-05-29 16:48:15 +0000

    Use switch to separate months

--- Lab4/Lab4/Calendar.cs
+++ Lab4/Lab4/Calendar.cs
@@ -17,58 +17,27 @@
             {
                 return -1;
             }
-
-            if (month == 1)
+            switch (month)
             {
-                return 31;
-            }
-            if (month == 2)
-            {
-                if (IsLeapYear(year))
-                {
-                    return 29;
-                }
-                return 28;
-            }
-            if (month == 3)
-            {
-                return 31;
-            }
-            if (month == 4)
-            {
-                return 30;
-            }
-            if (month == 5)
-            {
-                return 31;
-            }
-            if (month == 6)
-            {
-                return 30;
-            }
-            if (month == 7)
-            {
-                return 31;
-            }
-            if (month == 8)
-            {
-                return 31;
-            }
-            if (month == 9)
-            {
-                return 30;
-            }
-            if (month == 10)
-            {
-                return 31;
-            }
-            if (month == 11)
-            {
-                return 30;
-            }
-            if (month == 12)
-            {
-                return 31;
+                case 2:
+                    if (IsLeapYear(year))
+                    {
+                        return 29;
+                    }
+                    return 28;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    return 30;
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    return 31;
             }
             return -1;
         }
