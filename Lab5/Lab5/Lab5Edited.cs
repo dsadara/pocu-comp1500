8ac81324a9f07f4f20d421206b87270e13983e6d
lim971230 <lim971230@gmail.com>
2020-06-20 20:42:53 +0900

    Edit conditional statements

---
+++ Lab5/Lab5/Lab5Edited.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lab5
+{
+    public static class Lab5Edited
+    {
+        public static bool TryFixData(uint[] usersPerDay, double[] revenuePerDay)
+        {
+            double fixedRevenuePerDay = 0;
+            bool bRevenueFixed = false;
+            int i;
+            if (usersPerDay.Length != revenuePerDay.Length)
+            {
+                return false;
+            }
+            for (i = 0; i < usersPerDay.Length; ++i)
+            {
+                if (usersPerDay[i] < 0)
+                {
+                    Debug.Assert(false);
+                }
+                else if (usersPerDay[i] <= 10)
+                {
+                    fixedRevenuePerDay = usersPerDay[i] / (double)2;
+                }
+                else if (usersPerDay[i] <= 100)
+                {
+                    fixedRevenuePerDay = 16 * (double)usersPerDay[i] / 5 - 27;
+                }
+                else if (usersPerDay[i] <= 1000)
+                {
+                    fixedRevenuePerDay = usersPerDay[i] * (double)usersPerDay[i] / 4 - 2 * (double)usersPerDay[i] - 2007;
+                }
+                else
+                {
+                    fixedRevenuePerDay = 245743 + usersPerDay[i] / (double)4;
+                }       
+                fixedRevenuePerDay = (int)(fixedRevenuePerDay * 100 + 0.5) / 100.0; // 소수 둘째자리까지 반올림
+                if (fixedRevenuePerDay != revenuePerDay[i])
+                {
+                    bRevenueFixed = true;
+                    revenuePerDay[i] = fixedRevenuePerDay;
+                }
+            }
+            return bRevenueFixed;
+        }
+
+        public static int GetInvalidEntryCount(uint[] usersPerDay, double[] revenuePerDay)
+        {
+            int wrongElementNum = 0;
+            double fixedRevenuePerDay;
+            if (usersPerDay.Length == revenuePerDay.Length)
+            {
+                for (int i = 0; i < usersPerDay.Length; ++i)
+                {
+                    if (0 <= usersPerDay[i] && usersPerDay[i] <= 10)
+                    {
+                        fixedRevenuePerDay = usersPerDay[i] / (double)2;
+                    }
+                    else if (10 < usersPerDay[i] && usersPerDay[i] <= 100)
+                    {
+                        fixedRevenuePerDay = 16 * (double)usersPerDay[i] / 5 - 27;
+                    }
+                    else if (100 < usersPerDay[i] && usersPerDay[i] <= 1000)
+                    {
+                        fixedRevenuePerDay = usersPerDay[i] * (double)usersPerDay[i] / 4 - 2 * (double)usersPerDay[i] - 2007;
+                    }
+                    else
+                    {
+                        fixedRevenuePerDay = 245743 + usersPerDay[i] / (double)4;
+                    }
+                    fixedRevenuePerDay = Math.Round(fixedRevenuePerDay, 2);
+                    if (fixedRevenuePerDay != revenuePerDay[i])
+                    {
+                        ++wrongElementNum;
+                    }
+                }
+                return wrongElementNum;
+            }
+            return -1;
+        }
+
+        public static double CalculateTotalRevenue(double[] revenuePerDay, uint start, uint end)
+        {
+            int arrayLength = revenuePerDay.Length;
+            double totalRevenuePerDay = 0;
+            if (arrayLength != 0 && (0 <= start && start < arrayLength) && (0 <= end && end < arrayLength) && start <= end)
+            {
+                for (uint i = start; i <= end; ++i)
+                {
+                    totalRevenuePerDay += revenuePerDay[i];
+                }
+                return totalRevenuePerDay;
+            }
+            return -1;
+        }
+    }
+}
--- Lab5/Lab5/Program.cs
+++ Lab5/Lab5/Program.cs
@@ -11,14 +11,14 @@ namespace Lab5
             uint[] usersPerDay = new uint[NUMBER_OF_DAYS] { 0, 3, 5, 10, 34, 66, 89, 100, 101, 321, 657, 1000, 1032, 4520, 6578 };
             double[] revenuePerDay = new double[NUMBER_OF_DAYS] { 0, 1.50, 2.50, 5.00, 81.80, 184.20, 257.80, 293.00, 341.25, 23111.25, 104591.25, 245993.00, 246001.00, 246873.00, 247387.50 };
 
-            bool bFixed = Lab5.TryFixData(usersPerDay, revenuePerDay);
+            bool bFixed = Lab5Edited.TryFixData(usersPerDay, revenuePerDay);
             Debug.Assert(!bFixed);
 
             revenuePerDay[0] = 10000.2;
             revenuePerDay[6] = 156;
             revenuePerDay[11] = 45628.34;
 
-            bFixed = Lab5.TryFixData(usersPerDay, revenuePerDay);
+            bFixed = Lab5Edited.TryFixData(usersPerDay, revenuePerDay);
             Debug.Assert(bFixed);
 
             int numInvalidEntries = Lab5.GetInvalidEntryCount(usersPerDay, revenuePerDay);
@@ -43,8 +43,6 @@ namespace Lab5
             Debug.Assert(totalRevenue == 10000.2);
             totalRevenue = Lab5.CalculateTotalRevenue(revenuePerDay, 14, 14);
 
-
-            Debug.Assert(totalRevenue == 924657.79);
         }
     }
 }
