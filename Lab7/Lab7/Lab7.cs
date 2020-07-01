85dc431df17023861c2851c73560164416b70e2f
lim971230 <lim971230@gmail.com>
2020-07-02 01:19:23 +0900

    Change shift rule

--- Lab7/Lab7/Lab7.cs
+++ Lab7/Lab7/Lab7.cs
@@ -35,65 +35,6 @@
             return false;
         }
 
-        public static void ReachZeroRecursive(uint[] array, uint[] indexPassed, uint playerLocation, ref bool bPass, ref uint callCount)
-        {
-            // end conditions
-            if (bPass == true)
-            {
-                return;
-            }
-            if (array[playerLocation] == 0)
-            {
-                bPass = true;
-                return;
-            }
-            if (IsPassedIndex(playerLocation, indexPassed, callCount))
-            {
-                return;
-            }
-
-
-            indexPassed[callCount] = playerLocation;
-            callCount++;
-
-            uint[] nextLocation = new uint[array[playerLocation] + 1];
-            int temp;
-            for (int i = 0; i < array[playerLocation] + 1; i++)
-            {
-                
-                temp = (int)playerLocation + (i) - ((int)array[playerLocation] - i);
-                if (temp <= 0 || temp > array.Length - 1)
-                {
-                    continue;
-                }
-                if (array[temp] == 0)
-                {
-                    bPass = true;
-                    return; 
-                }
-                nextLocation[i] = (uint)temp;
-            }
-
-            // 점프할 수 있는 방법의 수 만큼 반복 
-            // 예) 2번 점프는 3가지 경우의 수 (오,오) (왼,오) (왼,왼)
-            for (uint i = 0; i < array[playerLocation] + 1; i++)
-            {
-                ReachZeroRecursive(array, indexPassed, nextLocation[i], ref bPass, ref callCount);
-            }
-            /*
-            for (uint i = 0; i < array[playerLocation] + 1; i++)
-            {
-                nextLocation = (int)playerLocation + (i) - ((int)array[playerLocation] - i);
-
-                if (nextLocation <= 0 || nextLocation > array.Length - 1)
-                {
-                    continue;
-                }
-                ReachZeroRecursive(array, indexPassed, nextLocation, ref bPass, ref callCount);
-            } 
-            */
-            return;
-        }
         public static void ReachZeroRecursive2(uint[] array, uint[] indexPassed, uint playerLocation, ref bool bPass, ref uint callCount)
         {
             // end conditions
@@ -115,18 +56,16 @@
             indexPassed[callCount] = playerLocation;
             callCount++;
 
-            uint nextLocation;
-
-            for (uint i = 0; i < array[playerLocation] + 1; i++)
+            uint nextLocationRightShift = playerLocation + array[playerLocation];
+            uint nextLocationLeftShift = playerLocation - array[playerLocation];
+            if (0 <= nextLocationRightShift && nextLocationRightShift <= array.Length - 1)
             {
-                nextLocation = playerLocation + (i) - (array[playerLocation] - i);
-
-                if (nextLocation <= 0 || nextLocation > array.Length - 1)
-                {
-                    continue;
-                }
-                ReachZeroRecursive(array, indexPassed, nextLocation, ref bPass, ref callCount);
-            } 
+                ReachZeroRecursive2(array, indexPassed, nextLocationRightShift, ref bPass, ref callCount);
+            }
+            if (0 <= nextLocationLeftShift && nextLocationLeftShift <= array.Length - 1)
+            {
+                ReachZeroRecursive2(array, indexPassed, nextLocationLeftShift, ref bPass, ref callCount);
+            }
             return;
         }
     }
